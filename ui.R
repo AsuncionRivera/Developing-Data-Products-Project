6c62db9a2ce06ab69c912f2691038477e7e975d2
AsuncionRivera <32930725+AsuncionRivera@users.noreply.github.com>
2019-01-11 08:15:28 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,19 @@
+
+library(shiny)
+
+
+
+shinyUI(fluidPage(
+  titlePanel("SALES SCORECARD"),
+  
+  sidebarLayout(
+    sidebarPanel(
+       selectInput("variable", "Display Sales for the Year : ", c("2017" = "YR2017",
+                                              "2018" = "YR2018"))
+    ),
+    mainPanel(
+       tableOutput("data")
+       
+    )
+  )
+))
